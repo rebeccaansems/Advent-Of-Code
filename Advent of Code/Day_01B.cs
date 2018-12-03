334de31aed783920ccecb8f0dc8710441d19514b
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-12-02 23:34:38 -0400

    Finished Day 1 part B

---
+++ Advent of Code/Day_01B.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code
+{
+    public class Day_01B
+    {
+        public void Run(List<string> data)
+        {
+            int freq = 0;
+            int dup = 0;
+            bool dupFound = false;
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            while (dupFound == false)
+            {
+                foreach (string instruction in data)
+                {
+                    freq += Int32.Parse(instruction);
+                    if (dict.ContainsKey(freq))
+                    {
+                        dupFound = true;
+                        dup = freq;
+                        break;
+                    }
+                    else
+                    {
+                        dict.Add(freq, 0);
+                    }
+                }
+            }
+            Console.WriteLine("Answer: " + dup);
+        }
+    }
+}
