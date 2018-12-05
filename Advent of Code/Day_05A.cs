3ffcd54c699d6eeeea27b68b6465097988faddc4
Rebecca Ansems <ansems_rebecca@hotmail.com>
2018-12-05 03:29:30 -0400

    Day 5

---
+++ Advent of Code/Day_05A.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code
+{
+    public class Day_05A
+    {
+        public void Run(List<string> data)
+        {
+            List<byte> polymer = Encoding.ASCII.GetBytes(data[0].ToCharArray()).ToList();
+
+            for (int i = 0; i < polymer.Count - 1; i++)
+            {
+                if (Math.Abs(polymer[i] - polymer[i + 1]) == 32)
+                {
+                    polymer.RemoveAt(i + 1);
+                    polymer.RemoveAt(i);
+
+                    i -= 2;
+                    if (i < 0)
+                    {
+                        i = 0;
+                    }
+                }
+            }
+
+            Console.WriteLine("Answer: " + polymer.Count);
+        }
+    }
+}
---
+++ Advent of Code/Day_05B.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code
+{
+    public class Day_05B
+    {
+        public void Run(List<string> data)
+        {
+            List<byte> basePolymer = Encoding.ASCII.GetBytes(data[0].ToCharArray()).ToList();
+
+            int shortestPoly = int.MaxValue;
+            for (int j = 65; j < 90; j++)
+            {
+                List<byte> polymer = Encoding.ASCII.GetBytes(data[0].ToCharArray()).ToList();
+
+                polymer.RemoveAll(x => x == j);
+                polymer.RemoveAll(x => x == j + 32);
+
+                for (int i = 0; i < polymer.Count - 1; i++)
+                {
+                    if (Math.Abs(polymer[i] - polymer[i + 1]) == 32)
+                    {
+                        polymer.RemoveAt(i + 1);
+                        polymer.RemoveAt(i);
+
+                        i -= 2;
+                        if (i < 0)
+                        {
+                            i = 0;
+                        }
+                    }
+                }
+
+                if (shortestPoly > polymer.Count)
+                {
+                    shortestPoly = polymer.Count;
+                }
+            }
+
+            Console.WriteLine("Answer: " + shortestPoly);
+        }
+    }
+}
